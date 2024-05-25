42ab9ec5fb1efd185d04dfe131bd29a874462235
Jorge Ferrero Linacero <jorgeferrerolinacero@gmail.com>
2024-05-25 18:16:41 +0200

    some tests

--- cmd/go-guru/main.go
+++ cmd/go-guru/main.go
@@ -1,8 +1,38 @@
 // Package main.
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/google/go-github/v62/github"
+	"github.com/gregjones/httpcache"
+	"github.com/sethvargo/go-githubactions"
+)
 
 func main() {
+	action := githubactions.New()
+
+	githubToken := action.GetInput("GITHUB_TOKEN")
+	if githubToken == "" {
+		githubactions.Fatalf("missing input 'GITHUB_TOKEN'")
+	}
+
+	repository := action.GetInput("GITHUB_REPOSITORY_NAME")
+	if repository == "" {
+		githubactions.Fatalf("missing input 'GITHUB_REPOSITORY_NAME'")
+	}
+
+	owner := action.GetInput("GITHUB_REPOSITORY_OWNER")
+	if owner == "" {
+		githubactions.Fatalf("missing input 'GITHUB_REPOSITORY_OWNER'")
+	}
+
+	action.AddMask(githubToken)
+
 	fmt.Println("Hello World From GitHub Action")
+
+	client := github.NewClient(
+		httpcache.NewMemoryCacheTransport().Client(),
+	).WithAuthToken(githubToken)
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/jferrl/go-guru
 
 go 1.22
+
+require (
+	github.com/google/go-github/v62 v62.0.0
+	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
+	github.com/sethvargo/go-githubactions v1.2.0
+)
+
+require github.com/google/go-querystring v1.1.0 // indirect
